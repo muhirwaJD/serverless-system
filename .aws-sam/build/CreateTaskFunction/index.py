0ef74b090409439fd54671f75d6f379ad79d1a93
muhirwJd <muhirwa232@gmail.com>
2025-07-10 12:13:13 +0200

    Installed boto3 for all the functions

---
+++ .aws-sam/build/CreateTaskFunction/index.py
@@ -0,0 +1,32 @@
+import json
+import boto3
+import uuid
+import os
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.environ["tasks"])
+
+def handler(event, context):
+    try:
+        body = json.loads(event["body"])
+        task_id = str(uuid.uuid4())
+        task_item = {
+            "id": task_id,
+            "title": body.get("title"),
+            "description": body.get("description"),
+            "assignedTo": body.get("assignedTo"),
+            "status": "pending"
+        }
+
+        table.put_item(Item=task_item)
+
+        return {
+            "statusCode": 201,
+            "body": json.dumps({"message": "Task created", "taskId": task_id})
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
